f7d7f5c264cfcc2942b7751ac1fc75f93a9806ac
Robson Wendel <robson.costa134@gmail.com>
2023-12-23 17:48:34 -0300

    componentizado

---
+++ app/src/components/TodoItems.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="space-y-2" :key="todo.id" v-for="todo in todosArray">
+    <TodoItem :todo="todo" />
+  </div>
+</template>
+<script>
+import TodoItem from "@/components/TodoItem.vue";
+
+export default {
+  name: 'TodoItems',
+  components: {
+    TodoItem
+  },
+  props: {
+    todosArray: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
